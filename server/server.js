3bc140f2d72855dcdc826efe24b6364a54e3eff0
Mike Wilcox <mwilcox56@gmail.com>
2015-05-14 21:42:33 -0400

    dont know how that was working before

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
 import utils from './modules/scraper';
 var express = require('express');
 
-const api = express()
+const app = express()
+  .all('/*', (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+    next()
+  })
   .get('/books', (req, res) => {
     utils.scrapeNYT('http://www.nytimes.com/best-sellers-books/').then((data) => {
       res.send(data);
     });
   });
 
-const app = express()
-  .all('/*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-    next()
-  });
-
-app.use('/api', api)
+app.use(express.static('.'))
   .listen(3001)
--- utils/api.ts
+++ utils/api.ts
@@ -1,8 +1,8 @@
 declare var fetch;
 
 export class API {
-	
+
 	getBooks() {
-		return fetch('http://localhost:3001/api/books')
+		return fetch('http://localhost:3001/books')
 	}
 }
